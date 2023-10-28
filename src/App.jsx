70c5719b5e1aebcbfa5611fbf1a1ab8c7640e23d
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-28 13:27:43 +0200

    BroswerRouter in main & Routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from "react";
 import "./App.css";
+import Homepage from "./pages/Homepage"
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return <>
+  <Routes>
+    <Route path="/" element={<Homepage/>}/>
+  </Routes>
+  </>;
 }
 
 export default App;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,9 @@
+
+const Homepage = () =>{
+    return(
+        <>
+        <h1>My homepage👋🏼</h1>
+        </>
+    )
+}
+export default Homepage;
