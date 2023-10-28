be14566051fb8b065080e73d1f3d708c6fabd46e
Marina Muñoz <marinacerejidoo@gmail.com>
2023-10-28 14:49:55 +0200

    Added a font and basic HTML for About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,6 +4,16 @@ const About = () => {
     <>
       
         <h1>About page🎀</h1>
+
+        <div className="first-panel">
+            <h3>Education</h3>
+            <p>I did a fullstack bootcamp at Ironhack and self-learned to expand my knowledge and skills</p>
+        </div>
+        <hr/>
+        <div className="second-panel">
+            <h3>Personal</h3>
+            <p>Chess and teaching Spanish online</p>
+        </div>
       
     </>
   );
